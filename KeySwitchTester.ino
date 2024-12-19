137113403531284d83cd7ec8c78c22e6c1d14fa2
Yong <yongjaewon@hotmail.com>
2024-12-19 18:43:03 -0500

    failure counts no longer stored on arduino. retrieve from nextion every time instead

--- KeySwitchTester.ino
+++ KeySwitchTester.ino
@@ -1,127 +1,125 @@
-#include "Nextion.h"
+#include <Nextion.h>
+
+Nextion *nex = Nextion::GetInstance(Serial2);
 
 // Define Nextion components
 
+NexPage activePage = NexPage(nex, 1, "active");
+NexPage popup0 = NexPage(nex, 2, "popup0");
+NexPage popup1 = NexPage(nex, 3, "popup1");
+NexPage popup2 = NexPage(nex, 4, "popup2");
+NexPage popup3 = NexPage(nex, 5, "popup3");
+
 NexNumber nTarget[] =
-{
-    NexNumber(1, 1, "nT0"),
-    NexNumber(1, 3, "nT1"),
-    NexNumber(1, 5, "nT2"),
-    NexNumber(1, 7, "nT3")
-};
+    {
+        NexNumber(nex, 1, 1, "nT0", &activePage),
+        NexNumber(nex, 1, 3, "nT1", &activePage),
+        NexNumber(nex, 1, 5, "nT2", &activePage),
+        NexNumber(nex, 1, 7, "nT3", &activePage)};
 
 NexNumber nCounter[] =
-{
-    NexNumber(1, 2, "nC0"),
-    NexNumber(1, 4, "nC1"),
-    NexNumber(1, 6, "nC2"),
-    NexNumber(1, 8, "nC3")
-};
+    {
+        NexNumber(nex, 1, 2, "nC0", &activePage),
+        NexNumber(nex, 1, 4, "nC1", &activePage),
+        NexNumber(nex, 1, 6, "nC2", &activePage),
+        NexNumber(nex, 1, 8, "nC3", &activePage)};
 
 NexCrop motorState[] =
-{
-    NexCrop(1, 10, "crM0"),
-    NexCrop(1, 12, "crM1"),
-    NexCrop(1, 14, "crM2"),
-    NexCrop(1, 16, "crM3")
-};
+    {
+        NexCrop(nex, 1, 10, "crM0", &activePage),
+        NexCrop(nex, 1, 12, "crM1", &activePage),
+        NexCrop(nex, 1, 14, "crM2", &activePage),
+        NexCrop(nex, 1, 16, "crM3", &activePage)};
 
 NexCrop keyState[] =
-{
-    NexCrop(1, 11, "crK0"),
-    NexCrop(1, 13, "crK1"),
-    NexCrop(1, 15, "crK2"),
-    NexCrop(1, 17, "crK3")
-};
+    {
+        NexCrop(nex, 1, 11, "crK0", &activePage),
+        NexCrop(nex, 1, 13, "crK1", &activePage),
+        NexCrop(nex, 1, 15, "crK2", &activePage),
+        NexCrop(nex, 1, 17, "crK3", &activePage)};
 
 NexNumber nMotorFails[] =
-{
-    NexNumber(1, 20, "nMF0"),
-    NexNumber(1, 21, "nMF1"),
-    NexNumber(1, 22, "nMF2"),
-    NexNumber(1, 23, "nMF3")
-};
+    {
+        NexNumber(nex, 1, 20, "nMF0", &activePage),
+        NexNumber(nex, 1, 21, "nMF1", &activePage),
+        NexNumber(nex, 1, 22, "nMF2", &activePage),
+        NexNumber(nex, 1, 23, "nMF3", &activePage)};
 
 NexNumber nKeyFails[] =
-{
-    NexNumber(1, 24, "nKF0"),
-    NexNumber(1, 25, "nKF1"),
-    NexNumber(1, 26, "nKF2"),
-    NexNumber(1, 27, "nKF3")
-};
+    {
+        NexNumber(nex, 1, 24, "nKF0", &activePage),
+        NexNumber(nex, 1, 25, "nKF1", &activePage),
+        NexNumber(nex, 1, 26, "nKF2", &activePage),
+        NexNumber(nex, 1, 27, "nKF3", &activePage)};
 
 NexDSButton stationEnable[] =
-{
-    NexDSButton(1, 28, "dsS0"),
-    NexDSButton(1, 29, "dsS1"),
-    NexDSButton(1, 30, "dsS2"),
-    NexDSButton(1, 31, "dsS3")
-};
+    {
+        NexDSButton(nex, 1, 28, "dsS0", &activePage),
+        NexDSButton(nex, 1, 29, "dsS1", &activePage),
+        NexDSButton(nex, 1, 30, "dsS2", &activePage),
+        NexDSButton(nex, 1, 31, "dsS3", &activePage)};
 
 NexNumber keyAmps[] =
-{
-    NexNumber(1, 35, "fAmps0"),
-    NexNumber(1, 36, "fAmps1"),
-    NexNumber(1, 37, "fAmps2"),
-    NexNumber(1, 38, "fAmps3")
-};
+    {
+        NexNumber(nex, 1, 35, "fAmps0", &activePage),
+        NexNumber(nex, 1, 36, "fAmps1", &activePage),
+        NexNumber(nex, 1, 37, "fAmps2", &activePage),
+        NexNumber(nex, 1, 38, "fAmps3", &activePage)};
 
 NexButton bReset[] =
-{
-    NexButton(2, 3, "b1"),
-    NexButton(3, 3, "b1"),
-    NexButton(4, 3, "b1"),
-    NexButton(5, 3, "b1")
-};
+    {
+        NexButton(nex, 2, 3, "b1", &popup0),
+        NexButton(nex, 3, 3, "b1", &popup1),
+        NexButton(nex, 4, 3, "b1", &popup2),
+        NexButton(nex, 5, 3, "b1", &popup3)};
 
-NexPage activePage = NexPage(1, 0, "active");
-NexDSButton dsEnable = NexDSButton(1, 9, "dsEnable");
-NexCrop sdState = NexCrop(1, 18, "crSD");
-NexText msg = NexText(1, 32, "tMsg");
-NexCrop battState = NexCrop(1, 33, "crVBatt");
-NexNumber vBatt = NexNumber(1, 34, "fVBatt");
-NexNumber cpm = NexNumber(1, 39, "nCPM");
+NexDSButton dsEnable = NexDSButton(nex, 1, 9, "dsEnable", &activePage);
+NexCrop sdState = NexCrop(nex, 1, 18, "crSD", &activePage);
+NexText msg = NexText(nex, 1, 32, "tMsg", &activePage);
+NexCrop battState = NexCrop(nex, 1, 33, "crVBatt", &activePage);
+NexNumber vBatt = NexNumber(nex, 1, 34, "fVBatt", &activePage);
+NexNumber cpm = NexNumber(nex, 1, 39, "nCPM", &activePage);
 
 // List of components to listen for events
 NexTouch *nex_listen_list[] =
-{
-    &bReset[0],
-    &bReset[1],
-    &bReset[2],
-    &bReset[3],
-    NULL
-};
+    {
+        &bReset[0],
+        &bReset[1],
+        &bReset[2],
+        &bReset[3],
+        NULL};
 
 // Servo and current sensor parameters
 const uint16_t T_DWELL = 600;
 const uint16_t SERVO_WAIT_TIME = 300;
 const uint16_t ZERO_POINT[4] = {565, 525, 528, 595}; // Zero points for each station
-const float V_REF = 5.0;                        // Arduino reference voltage
-const float ADC_RESOLUTION = 1023.0;            // 10-bit ADC resolution
-const float V_OFFSET = 2.5;                     // Zero-current voltage (2.5V)
-const float SENSITIVITY = 0.04;                 // Sensitivity in V/A (40 mV/A for ACS758-050B)
-const uint8_t AMPS_PIN = A0;                        // Current sensor connected to A0
-const uint8_t AMPS_THRESHOLD = 5;                   // Failure threshold: 5A
-const uint8_t FAILURE_THRESHOLD = 10;                 // Total number of failures threshold: 10
-const uint8_t AMPS_N_TOP = 5;                      // Number of top readings to maintain
+const float V_REF = 5.0;                             // Arduino reference voltage
+const float ADC_RESOLUTION = 1023.0;                 // 10-bit ADC resolution
+const float V_OFFSET = 2.5;                          // Zero-current voltage (2.5V)
+const float SENSITIVITY = 0.04;                      // Sensitivity in V/A (40 mV/A for ACS758-050B)
+const uint8_t AMPS_PIN = A0;                         // Current sensor connected to A0
+const uint8_t AMPS_THRESHOLD = 5;                    // Failure threshold: 5A
+const uint8_t FAILURE_THRESHOLD = 10;                // Total number of failures threshold: 10
+const uint8_t AMPS_N_TOP = 5;                        // Number of top readings to maintain
 const uint8_t STATE_DELAY = 50;
 const uint8_t V_BATT_PIN = A2;
 
-uint16_t stationDelay = 0;             // Time delay between stations
-unsigned long tlastCycle = 0;                   // Timer to track periodic updates from Nextion
-bool masterEnable = false;                      // Master enable state
+uint16_t stationDelay = 0;    // Time delay between stations
+unsigned long tlastCycle = 0; // Timer to track periodic updates from Nextion
+bool masterEnable = false;    // Master enable state
 
-float topReadings[AMPS_N_TOP];                  // Array to store top N readings
-uint8_t topCount = 0;                               // Current number of readings in topReadings
+float topReadings[AMPS_N_TOP]; // Array to store top N readings
+uint8_t topCount = 0;          // Current number of readings in topReadings
 
 uint32_t measurementCounter = 0;
 
 // Display state colors
-enum Color {
-  GRAY = 0,
-  GREEN = 1,
-  ORANGE = 2,
-  RED = 3
+enum Color
+{
+    GRAY = 0,
+    GREEN = 1,
+    ORANGE = 2,
+    RED = 3
 };
 
 // State machine states
@@ -148,11 +146,11 @@ ServoSubState servoSubState = SERVO_START; // Initial substate for servo command
 unsigned long lastActuationTime = 0; // Timer to track periodic actuation
 unsigned long lastStateTime = 0;     // Timer for state transitions
 
-bool enabledStations[4] = {0, 0, 0, 0};     // Store which stations are enabled
-uint8_t numEnabledStations = 0;             // Count of enabled stations
-uint8_t currentStationIndex = -1;               // Current station being processed
+bool enabledStations[4] = {0, 0, 0, 0}; // Store which stations are enabled
+uint8_t numEnabledStations = 0;         // Count of enabled stations
+uint8_t currentStationIndex = -1;       // Current station being processed
 
-uint32_t failureCounts[4] = {8, 3, 1, 0};
+// uint32_t failureCounts[4] = {8, 4, 1, 0};
 
 void insertTopReading(float current)
 {
@@ -161,11 +159,11 @@ void insertTopReading(float current)
     {
         topReadings[topCount] = current;
         topCount++;
-        
+
         // Sort the array in descending order
-        for(int8_t i = topCount - 1; i > 0; i--)
+        for (int8_t i = topCount - 1; i > 0; i--)
         {
-            if(topReadings[i] > topReadings[i - 1])
+            if (topReadings[i] > topReadings[i - 1])
             {
                 float temp = topReadings[i];
                 topReadings[i] = topReadings[i - 1];
@@ -180,14 +178,14 @@ void insertTopReading(float current)
     else
     {
         // Compare with the smallest (last element)
-        if(current > topReadings[AMPS_N_TOP - 1])
+        if (current > topReadings[AMPS_N_TOP - 1])
         {
             topReadings[AMPS_N_TOP - 1] = current;
-            
+
             // Move the new reading up to maintain descending order
-            for(int i = AMPS_N_TOP - 1; i > 0; i--)
+            for (int i = AMPS_N_TOP - 1; i > 0; i--)
             {
-                if(topReadings[i] > topReadings[i - 1])
+                if (topReadings[i] > topReadings[i - 1])
                 {
                     float temp = topReadings[i];
                     topReadings[i] = topReadings[i - 1];
@@ -256,7 +254,8 @@ void updateMasterEnable()
         }
     }
 
-    if (success) masterEnable = (bool)isEnabled;
+    if (success)
+        masterEnable = (bool)isEnabled;
 }
 
 // Function to read current and update the top readings
@@ -276,7 +275,7 @@ uint32_t calculateAvgAmps()
     {
         return 0;
     }
-    
+
     // Calculate the sum of the top readings
     float sum = 0.0f;
     for (uint8_t i = 0; i < topCount; i++)
@@ -284,10 +283,12 @@ uint32_t calculateAvgAmps()
         sum += topReadings[i];
     }
     float average = sum / topCount;
-    
-    if (average < 0) return 0;
+
+    if (average < 0)
+        return 0;
     // Multiply by 10 to shift decimal, add 0.5 for rounding, and cast to int
-    else return (uint32_t)(average * 10.0f + 0.5f); // Returns the average value as an integer in tenth of an Amp
+    else
+        return (uint32_t)(average * 10.0f + 0.5f); // Returns the average value as an integer in tenth of an Amp
 }
 
 // Update enabled stations
@@ -313,8 +314,9 @@ void updateEnabledStations()
 
         if (success)
         {
-          enabledStations[i] = isEnabled;
-          if (isEnabled) numEnabledStations++;
+            enabledStations[i] = isEnabled;
+            if (isEnabled)
+                numEnabledStations++;
         }
     }
 }
@@ -325,19 +327,26 @@ void calculateAmps()
     // Serial.println(avgCurrent);
     for (uint8_t i = 0; i < 3; i++)
     {
-      if (keyAmps[currentStationIndex].setValue(avgCurrent)) break;
+        if (keyAmps[currentStationIndex].setValue(avgCurrent))
+            break;
     }
 
     // Serial.print("Current: ");
     // Serial.println(avgCurrent);
 
+
+    uint32_t fails;
+    nKeyFails[currentStationIndex].getValue(&fails);
     if (avgCurrent < 50) // If less than 5.0 Amps
     {
-        failureCounts[currentStationIndex]++;
-        for (uint8_t i = 0; i < 4; i++)
-        {
-            if (nKeyFails[currentStationIndex].setValue(failureCounts[currentStationIndex])) break;
-        }
+        fails++;
+        nKeyFails[currentStationIndex].setValue(fails);
+        // failureCounts[currentStationIndex]++;
+        // for (uint8_t i = 0; i < 4; i++)
+        // {
+        //     if (nKeyFails[currentStationIndex].setValue(failureCounts[currentStationIndex]))
+        //         break;
+        // }
     }
 
     // Serial.print(failureCounts[currentStationIndex]);
@@ -346,12 +355,14 @@ void calculateAmps()
     // Serial.print("?: ");
     // Serial.println(failureCounts[currentStationIndex] >= FAILURE_THRESHOLD? "true" : "false");
 
-    if (failureCounts[currentStationIndex] >= FAILURE_THRESHOLD)
+    if (fails >= FAILURE_THRESHOLD)
     {
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        if (stationEnable[currentStationIndex].setValue(0)) break;
-      } 
+        // for (uint8_t i = 0; i < 3; i++)
+        // {
+        //     if (stationEnable[currentStationIndex].setValue(0))
+        //         break;
+        // }
+        stationEnable[currentStationIndex].setValue(0);
     }
 
     resetAmps();
@@ -368,7 +379,7 @@ void resetAmps()
 
 void handleStates()
 {
-    static uint32_t currentCount = 0;   // Track the current count locally
+    static uint32_t currentCount = 0; // Track the current count locally
 
     switch (currentState)
     {
@@ -376,13 +387,14 @@ void handleStates()
         if (millis() - lastActuationTime >= stationDelay)
         {
             updateEnabledStations();
-            updateStationDelay(); 
+            updateStationDelay();
             if (numEnabledStations > 0)
             {
-                for(int i = 0; i < 4; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     currentStationIndex = (currentStationIndex + 1) % 4;
-                    if(enabledStations[currentStationIndex]) break;
+                    if (enabledStations[currentStationIndex])
+                        break;
                 }
                 currentState = GET_VALUE;
                 lastActuationTime = millis();
@@ -447,8 +459,8 @@ void handleStates()
         // Serial.println(measurementCounter);
         for (int i = 0; i < AMPS_N_TOP; i++)
         {
-          Serial.print(topReadings[i]);
-          Serial.print(", ");
+            Serial.print(topReadings[i]);
+            Serial.print(", ");
         }
         Serial.println();
         calculateAmps();
@@ -487,26 +499,30 @@ void setup()
     // Initialize Serial1 for Hiwonder Servo Controller
     Serial1.begin(9600);
 
-    // nexSerial for Nextion defaults to Serial2
-    nexSerial.begin(115200);
+    // // nexSerial for Nextion defaults to Serial2
+    // nexSerial.begin(115200);
+    if(!nex->nexInit(115200))
+    {
+        Serial.println("nextion init fails"); 
+    }
     delay(500);
 
     // Retry mechanism for Nextion initialization
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        if (nexInit())
-        {
-            break;
-        }
-        delay(50); // Small delay before retrying
-    }
+    // for (uint8_t i = 0; i < 3; i++)
+    // {
+    //     if (nexInit())
+    //     {
+    //         break;
+    //     }
+    //     delay(50); // Small delay before retrying
+    // }
 
     for (uint8_t i = 0; i < 4; i++)
     {
-      for (uint8_t j = 0; j < 4; j++)
-      {
-        bReset[i].attachPop(resetStation, &bReset[i]);
-      }
+        for (uint8_t j = 0; j < 4; j++)
+        {
+            bReset[i].attachPop(resetStation, &bReset[i]);
+        }
     }
 
     for (uint8_t i = 0; i < 3; i++)
@@ -518,13 +534,14 @@ void setup()
         delay(50); // Small delay before retrying
     }
 
-    for (uint8_t i = 0; i < 4; i++)
-    {
-      for (uint8_t j = 0; j < 3; j++)
-      {
-          if (nKeyFails[i].setValue(failureCounts[i])) break;
-      }
-    } 
+    // for (uint8_t i = 0; i < 4; i++)
+    // {
+    //     for (uint8_t j = 0; j < 3; j++)
+    //     {
+    //         if (nKeyFails[i].setValue(failureCounts[i]))
+    //             break;
+    //     }
+    // }
 }
 
 void resetStation(NexButton *button)
@@ -532,33 +549,37 @@ void resetStation(NexButton *button)
     Serial.print("Resetting station ");
     uint8_t stationToReset = button->getObjPid() - 2; // Station 0: Page 2, Station 1: Page 3, etc...
     Serial.println(stationToReset + 1);
-    failureCounts[stationToReset] = 0;
-    for (uint8_t i = 0; i < 4; i++)
-    {
-      Serial.print(failureCounts[i]);
-      Serial.print(", ");
-    }
-    Serial.println();
+    nKeyFails[stationToReset].setValue(0);
+    // failureCounts[stationToReset] = 0;
+    // for (uint8_t i = 0; i < 4; i++)
+    // {
+    //     Serial.print(failureCounts[i]);
+    //     Serial.print(", ");
+    // }
+    // Serial.println();
     updateEnabledStations();
 }
 
 void updateVBatt()
 {
-  float vBattFraction = analogRead(V_BATT_PIN) / ADC_RESOLUTION;
-  uint32_t vBattInt = (uint32_t)(vBattFraction * 650 * 1.041 / 3 + 0.5);
-  for (int i = 0; i <3; i++)
-  {
-    if (vBatt.setValue(vBattInt)) break;
-  }
+    float vBattFraction = analogRead(V_BATT_PIN) / ADC_RESOLUTION;
+    uint32_t vBattInt = (uint32_t)(vBattFraction * 650 * 1.041 / 3 + 0.5);
+    for (int i = 0; i < 3; i++)
+    {
+        if (vBatt.setValue(vBattInt))
+            break;
+    }
 }
 
 void loop()
 {
-    nexLoop(nex_listen_list);
+    nex->nexLoop(nex_listen_list);
     updateMasterEnable();
     updateVBatt();
-    if (masterEnable) handleStates();
-    else goToSafeState();
+    if (masterEnable)
+        handleStates();
+    else
+        goToSafeState();
 }
 
 void goToSafeState()
