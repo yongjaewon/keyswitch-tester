f2f763db629a3642252794dfc5bc76d663463e8a
Yongjae Won <yongjaewon@hotmail.com>
2025-05-08 11:47:50 -0400

    Remove old directory structure files

--- keyswitch-tester-arduino/currentMeasurer.h
+++
@@ -1,7 +0,0 @@
-//currentMeasurer.h
-#pragma once
-#include "config.h"
-
-void trackPeakCurrent();
-float getPeakCurrent();
-void resetPeakCurrent();
--- keyswitch-tester-arduino/eventReporter.h
+++
@@ -1,6 +0,0 @@
-//eventReporter.h
-#pragma once
-#include <Arduino.h>
-
-void reportCurrent(int station, float current);
-void reportEvent(const String& message);
